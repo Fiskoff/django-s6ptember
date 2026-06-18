47af47aa073423d57cedf387bbcf48fa5d5e06a6
fiskov <fiskoff2015@yandex.ru>
2026-06-19 00:14:29 +0700

    feat: create ORM models for category and products

--- shop/main/models.py
+++ shop/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:  # Настройки модели в админке
+        ordering = ('name',)  # Сортировка в админке по полю
+        verbose_name = 'Категория'  # Название в админке
+        verbose_name_plural = 'Категории' # Название в админке во множественном числе
+
+    def  __str__(self):  # Отображение объекта котегории в админке
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)  # Связь между продуктами и категориями
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)  # upload_to - папка куда мы будем загружать картинки; blank=True - поле может быть пустое
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def  __str__(self):
+        return self.name
